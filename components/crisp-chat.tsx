f68f60afb8e795f221d732cf56487538f2ce8b11
Investe USA <75433200+investeusa@users.noreply.github.com>
2024-02-04 00:03:11 -0300

    crisp

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -3,9 +3,11 @@
 import { useEffect } from "react";
 import { Crisp } from "crisp-sdk-web";
 
+const CRISP_ID = process.env.CRISP_ID?.toString();
+
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("77552ade-e505-403e-baca-8b1e9fe5523b");
   }, []);
 
   return null;
